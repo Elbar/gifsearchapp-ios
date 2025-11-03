6827de97ec7ffde003138395b818913b866da51f
Elbar <Elbar.israilov@mudita.com>
2025-11-04 00:12:15 +0600

    first commit

---
+++ GifSearchAppTests/GifSearchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GifSearchAppTests.swift
+//  GifSearchAppTests
+//
+//  Created by Elbar on 4/11/25.
+//
+
+import Testing
+@testable import GifSearchApp
+
+struct GifSearchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# gifsearchapp-ios
